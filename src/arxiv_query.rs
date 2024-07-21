1ffa6119887e20cd5acbda07491b5715e0e8296f
Alexis Morvan <morvan.alexis@gmail.com>
2024-07-20 22:01:28 -0700

    clean up the construction of the query string

--- src/arxiv_query.rs
+++ src/arxiv_query.rs
@@ -1,29 +1,207 @@
+//! Query interface for the [`arXiv API`]
+//!
+//! This module provide the necessary tool to query (using reqwest) and pasrse
+//! the arxiv XML file and cast it into a struct found in arxiv_entry module.
+//!
+//! Context for the API:
+//! The parameters for each of the API has a base url that reads:
+//! `http://export.arxiv.org/api/query?{parameters}`
+//!
+//! The parameters are separated with a `&` in the construction of the url. Here are the one
+//! useable in this module:
+//! - search_query : Search query to find article,
+//! - id_list: a list of id to search from,
+//! - start and max_results allows to download chunk of the data.
+//!
+//!
+//! [`arXiv API`] : https://info.arxiv.org/help/api/user-manual.html
+
 use crate::arxiv_entry::ArxivEntry;
 use minidom::Element;
+use std::fmt::Display;
 use std::{error::Error, str::FromStr};
 
 const ENTRY_NS: &'static str = "http://www.w3.org/2005/Atom";
+const ARXIV_QUERY_BASE_URL: &'static str = "http://export.arxiv.org/api/query?";
+const DEFAULT_MAX_RESULTS: usize = 200;
+const DEFAULT_SORT_BY: SortBy = SortBy::SubmittedDate;
+const DEFAULT_SORT_ORDER: SortOrder = SortOrder::Descending;
+
+enum SortBy {
+    Relevance,
+    LastUpdatedDate,
+    SubmittedDate,
+}
+
+enum SortOrder {
+    Ascending,
+    Descending,
+}
+
+impl Display for SortBy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SortBy::Relevance => "relevance",
+                SortBy::LastUpdatedDate => "lastUpdatedDate",
+                SortBy::SubmittedDate => "submittedDate",
+            }
+        )
+    }
+}
+
+impl Display for SortOrder {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SortOrder::Ascending => "ascending",
+                SortOrder::Descending => "descending",
+            }
+        )
+    }
+}
+
+fn get_search_str(
+    category: Option<&str>,
+    author: Option<&str>,
+    start_index: Option<usize>,
+    max_results: Option<usize>,
+    sort_by: Option<SortBy>,
+    sort_order: Option<SortOrder>,
+) -> String {
+    let mut search_str = String::new();
+
+    if let Some(cat) = category {
+        search_str.push_str(&format!("&cat:{}", cat));
+    }
+
+    if let Some(auth) = author {
+        search_str.push_str(&format!("&au:{}", auth));
+    }
+
+    if let Some(start) = start_index {
+        search_str.push_str(&format!("&start={}", start))
+    }
+
+    if let Some(max_res) = max_results {
+        search_str.push_str(&format!("&max_results={}", max_res))
+    }
+
+    if let Some(sort_by) = sort_by {
+        search_str.push_str(&format!("&sortBy={}", sort_by))
+    }
+    if let Some(sort_order) = sort_order {
+        search_str.push_str(&format!("&sortOrder={}", sort_order))
+    }
+
+    // If the string search str is empty, we return an empty string:
+    if search_str.len() == 0 {
+        search_str
+    } else {
+        format!("search_query={}", search_str)
+    }
+}
+
+/// Constructs a URL string for querying the arXiv archive based on search parameters.
+///
+/// This function takes various optional parameters to construct a URL string suitable
+/// for querying the arXiv archive.
+///
+/// Parameters:
+///
+/// * `category`: An optional arXiv category to search within.
+/// * `author`: An optional reference to a string slice representing the author's name.
+/// * `start_index`: An optional `usize` representing the starting index for search results.
+/// * `max_results`: An optional `usize` representing the maximum number of results to return.
+///     * If not provided, a default value (`DEFAULT_MAX_RESULTS`) is used.
+/// * `sort_by`: An optional `SortBy` enum variant specifying the sorting criteria for results.
+///     * Defaults to `DEFAULT_SORT_BY` if not provided.
+/// * `sort_order`: An optional `SortOrder` enum variant specifying the sorting order (ascending or descending).
+///     * Defaults to the default order associated with the chosen `sort_by` option.
+fn get_query_url_str(
+    category: Option<&str>,
+    author: Option<&str>,
+    start_index: Option<usize>,
+    max_results: Option<usize>,
+    sort_by: Option<SortBy>,
+    sort_order: Option<SortOrder>,
+) -> String {
+    let search_str = get_search_str(
+        category,
+        author,
+        start_index,
+        max_results,
+        sort_by,
+        sort_order,
+    );
+    format!("{}{}", ARXIV_QUERY_BASE_URL, search_str)
+}
+
+#[test]
+fn test_get_search_str_basic() {
+    let url = get_query_url_str(None, None, None, None, None, None);
+    assert_eq!(url, ARXIV_QUERY_BASE_URL);
+}
+
+#[test]
+fn test_get_search_str_category() {
+    let url = get_query_url_str(Some("cs.AI"), None, None, None, None, None);
+    assert_eq!(
+        url,
+        format!("{}search_query=&cat:cs.AI", ARXIV_QUERY_BASE_URL)
+    );
+}
+
+#[test]
+fn test_get_search_str_author() {
+    let url = get_query_url_str(None, Some("Albert Einstein"), None, None, None, None);
+    assert_eq!(
+        url,
+        format!("{}search_query=&au:Albert Einstein", ARXIV_QUERY_BASE_URL)
+    );
+}
+
+#[test]
+fn test_get_search_str_all_params() {
+    let url = get_query_url_str(
+        Some("stat.ML"),
+        Some("Jane Doe"),
+        Some(10),
+        Some(50),
+        Some(SortBy::LastUpdatedDate),
+        Some(SortOrder::Descending),
+    );
+    assert_eq!(
+    url,
+    format!(
+      "{}search_query=&cat:stat.ML&au:Jane Doe&start=10&max_results=50&sortBy=lastUpdatedDate&sortOrder=descending",
+      ARXIV_QUERY_BASE_URL
+    )
+  );
+}
 
 /// Constructs a URL string for querying arXiv based on search parameters.
-fn get_search_query_str(
+fn get_query_str(
     category: &str,
     start_index: usize,
     max_results: usize,
 ) -> Result<String, Box<dyn Error>> {
-    let base_url = "http://export.arxiv.org/api/query?";
-
     let query_string = format!(
         "search_query={}&sortBy=lastUpdatedDate&start={}&max_results={}",
         category, start_index, max_results
     );
 
-    Ok(format!("{}{}", base_url, query_string))
+    Ok(format!("{}{}", ARXIV_QUERY_BASE_URL, query_string))
 }
 
 #[test]
-fn test_get_search_query_str() -> Result<(), Box<dyn Error>> {
+fn test_get_query_str() -> Result<(), Box<dyn Error>> {
     let expected_url = "http://export.arxiv.org/api/query?search_query=cat:quant-ph&sortBy=lastUpdatedDate&start=0&max_results=200";
-    let actual_url = get_search_query_str("cat:quant-ph", 0, 200)?;
+    let actual_url = get_query_str("cat:quant-ph", 0, 200)?;
 
     assert_eq!(expected_url, actual_url);
 
@@ -31,8 +209,8 @@ fn test_get_search_query_str() -> Result<(), Box<dyn Error>> {
 }
 
 pub fn query_arxiv() -> Result<String, Box<dyn Error>> {
-    let search_query_str = get_search_query_str("cat:quant-ph", 0, 200)?;
-    Ok(reqwest::blocking::get(search_query_str)?.text()?)
+    let query_str = get_query_str("cat:quant-ph", 0, 200)?;
+    Ok(reqwest::blocking::get(query_str)?.text()?)
 }
 
 pub fn parse_arxiv_entries(content: &str) -> Result<Vec<ArxivEntry>, Box<dyn Error>> {
