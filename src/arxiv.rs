fba06dfe1ab14ad36eb1bde499999b66c296008a
Alexis Morvan <morvan.alexis@gmail.com>
2024-07-29 21:29:10 -0700

    Clean up: Config for higlhlight + moving srxiv into subfolder

---
+++ src/arxiv.rs
@@ -0,0 +1,5 @@
+mod parsing;
+mod query;
+
+pub use parsing::*;
+pub use query::*;
--- src/event.rs
+++ src/event.rs
@@ -4,13 +4,10 @@ use ratatui::crossterm::event::{
 };
 use std::sync::mpsc;
 use std::thread;
-use std::time::{Duration, Instant};
 
 /// Terminal events.
 #[derive(Clone, Copy, Debug)]
 pub enum Event {
-    /// Terminal tick.
-    Tick,
     /// Key press.
     Key(KeyEvent),
     /// Mouse click/scroll.
@@ -33,41 +30,26 @@ pub struct EventHandler {
 
 impl EventHandler {
     /// Constructs a new instance of [`EventHandler`].
-    pub fn new(tick_rate: u64) -> Self {
-        let tick_rate = Duration::from_millis(tick_rate);
+    pub fn new() -> Self {
         let (sender, receiver) = mpsc::channel();
         let handler = {
             let sender = sender.clone();
-            thread::spawn(move || {
-                let mut last_tick = Instant::now();
-                loop {
-                    let timeout = tick_rate
-                        .checked_sub(last_tick.elapsed())
-                        .unwrap_or(tick_rate);
-
-                    if event::poll(timeout).expect("failed to poll new events") {
-                        match event::read().expect("unable to read event") {
-                            CrosstermEvent::Key(e) => {
-                                if e.kind == KeyEventKind::Press {
-                                    sender.send(Event::Key(e))
-                                } else {
-                                    Ok(())
-                                }
-                            }
-                            CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
-                            CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
-                            CrosstermEvent::FocusGained => Ok(()),
-                            CrosstermEvent::FocusLost => Ok(()),
-                            CrosstermEvent::Paste(_) => unimplemented!(),
+            thread::spawn(move || loop {
+                match event::read().expect("unable to read event") {
+                    CrosstermEvent::Key(e) => {
+                        if e.kind == KeyEventKind::Press {
+                            sender.send(Event::Key(e))
+                        } else {
+                            Ok(())
                         }
-                        .expect("failed to send terminal event")
                     }
-
-                    if last_tick.elapsed() >= tick_rate {
-                        sender.send(Event::Tick).expect("failed to send tick event");
-                        last_tick = Instant::now();
-                    }
+                    CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
+                    CrosstermEvent::Resize(w, h) => sender.send(Event::Resize(w, h)),
+                    CrosstermEvent::FocusGained => Ok(()),
+                    CrosstermEvent::FocusLost => Ok(()),
+                    CrosstermEvent::Paste(_) => unimplemented!(),
                 }
+                .expect("failed to send terminal event")
             })
         };
         Self {
